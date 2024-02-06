e1d9b16f469c2612f44baa987f8515b7bb947369
Chhatrapalsinh Zala <chhatrapalsinh.zala@nitorinfotech.com>
2024-02-06 11:07:49 +0530

    Initial dates in all models.

--- nitoronlinetestportal/tests_app/models.py
+++ nitoronlinetestportal/tests_app/models.py
@@ -2,8 +2,11 @@ import uuid
 
 from django.db import models
 
+from questions.basemodel import BaseModel
+from django.contrib.auth.models import User
 
-class TestsDetails(models.Model):
+
+class TestsDetails(BaseModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=500, null=True)
     total_questions = models.IntegerField(default=0)
@@ -12,17 +15,22 @@ class TestsDetails(models.Model):
     duration = models.DurationField(null=True)
     weightage = models.IntegerField(null=True, blank=True)
     end_date = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name="test_detail_created_by", null=True, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name="test_detail_updated_by", null=True, on_delete=models.CASCADE)
 
 
-class TestAllocations(models.Model):
+
+class TestAllocations(BaseModel):
     name = models.CharField(max_length=500, null=True)
     key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
     test = models.ForeignKey(TestsDetails, related_name='test_allocations', null=False, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name="test_allocations_created_by", null=True, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name="test_allocations_updated_by", null=True, on_delete=models.CASCADE)
 
 
-class UserTests(models.Model):
+class UserTests(BaseModel):
     first_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50, null=True)
     email = models.CharField(max_length=500, null=True)
@@ -33,3 +41,5 @@ class UserTests(models.Model):
     generated_question = models.JSONField(default=list)
     submission_date = models.DateTimeField(auto_now=False, blank=True, null=True)
     captured_image_locations = models.JSONField(max_length=10000, default=list, null=True)
+    created_by = models.ForeignKey(User, related_name="user_tests_created_by", null=True, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name="user_tests_updated_by", null=True, on_delete=models.CASCADE)
